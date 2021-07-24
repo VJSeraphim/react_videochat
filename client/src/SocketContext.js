f64d446d7f209d72173b72aa8845d7136e9db6e3
VJS <minsung_96@naver.com>
2021-07-24 23:55:13 +0900

    answercall function

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -10,8 +10,12 @@ const ContextProvider = ({children}) => {
     const [stream, setStream] = useState(null)
     const [me, setMe] = useState('')
     const [call, setCall] = useState(null)
+    const [callAccepted, setCallAccepted] = useState(false)
+    const [callFinished, setCallFinished] = useState(false)
 
     const myVideo = useRef()
+    const userVideo = useRef()
+    const connectionRect = useRef()
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true})
@@ -27,7 +31,24 @@ const ContextProvider = ({children}) => {
     }, [])
 
     const answerCall = () => {
+        setCallAccepted(true)
+        const peer = new Peer({
+            initiator: false,
+            trickle: false,
+            stream,
+        })
 
+        peer.on('signal', (data) => {
+            socket.emit('answercall', {signal: data, to: call.from})
+        })
+
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        peer.signal(call.signal)
+
+        connectionRef.current = peer
     }
 
     const callUser = () => {
